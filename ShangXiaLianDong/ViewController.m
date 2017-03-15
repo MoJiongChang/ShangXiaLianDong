03ad56989c29f8398c61d4eb5f0bc45eedb6f93c
MoJiongChang <gdshwhl007@gdshwhl007demac-mini.local>
2017-03-15 19:07:20 +0800

    Initial Commit

---
+++ ShangXiaLianDong/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ShangXiaLianDong
+//
+//  Created by gdshwhl007 on 17/3/15.
+//  Copyright © 2017年 莫炯昌. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ShangXiaLianDong/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ShangXiaLianDong
+//
+//  Created by gdshwhl007 on 17/3/15.
+//  Copyright © 2017年 莫炯昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
